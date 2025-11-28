44fc699907dd9a77972f062cdec6ada2dcdb0dd2
Alimzy <salamialameen20@gmail.com>
2025-11-28 16:08:26 +0100

    Assignment

---
+++ mathematics.js
@@ -0,0 +1,26 @@
+let firstVariable = 2
+let secondVariable = 4
+
+let sum = firstVariable  + secondVariable
+
+let sumTwo = sum * 2
+
+console.log(sumTwo)
+
+
+
+let number = 4
+
+let division = 4 % 10
+
+let addition = number + division
+
+console.log(addition)
+
+
+
+let valueOne = 7
+let  valueTwo = 2
+let multiplication = valueOne *(valueOne * valueTwo)
+
+console.log(multiplication)
