17c9c3fb749285a85663fd9c1dc2af643dc66468
Alimzy <salamialameen20@gmail.com>
2026-01-28 08:27:11 +0100

    commit

--- Morning Snack/Java/ArmstrongNumber.java
+++ Morning Snack/Java/ArmstrongNumber.java
@@ -1,1 +1,42 @@
+public class ArmstrongNumber{
+ public static void main(String... args){
+System.out.println(isArmstrong(145));
+}
+
+public static boolean isArmstrong(int number){
+   int originalNumber = number;
+   String newNumber = number + "";
+   int length = newNumber.length();
+   int multiplication = 1;
+   int sum = 0;
+    
+    while(length > 1){
+    int lastDigit = number % 10;
+        number = number / 10;
+
+    for(int count = 1;count < length;count++){
+    multiplication = multiplication * count;
+    sum += multiplication;
+    multiplication = 1;
+
+        }
+
+}
+
+if(sum == originalNumber){
+return true;
+}else{
+return false;
+}
+
+   
+
+
+
+
+}
+
+
+
+}
 
